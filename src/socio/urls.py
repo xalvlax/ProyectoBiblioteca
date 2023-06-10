ddd606622da3e16baa644538bacc468a82fc88fb
xalvlax <sergiosalvatierra86@gmail.com>
2023-06-10 00:21:58 -0300

    ADD: Se crea repositorio del Proyecto de Biblioteca

--- src/socio/urls.py
+++ src/socio/urls.py
@@ -12,7 +12,7 @@ app_name = 'socios'
 urlpatterns = [
 	path('nuevo/', CrearSocio.as_view(), name= 'CrearSocio'),
 	path('listar/', ListarSocios.as_view(), name= 'ListarSocios'),
-	path('modificar/<int:pk>/', EditarSocio.as_view(), name='EditarSocio'),
+	path('modificar/<int:pk>/', EditarSocio.as_view(), name= 'EditarSocio'),
 	path('activar/<int:id>/', activar_socio, name= 'activar_socio'),
 	path('desactivar/<int:id>/', desactivar_socio, name= 'desactivar_socio'),
 ]
