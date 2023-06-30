54499dcd6d9b41eda2add590d0058ed51d68a0d8
xalvlax <sergiosalvatierra86@gmail.com>
2023-06-29 23:57:31 -0300

    FIX: Se corrige y modifican todos los validadores de nombre de cada app

--- src/libro/validators.py
+++ src/libro/validators.py
@@ -2,15 +2,20 @@ from django.core.exceptions import ValidationError
 
 def nombreValidator(value):
 	# value va a tener todo el string que se introdujo en el input
-	# voy a separar por cada espacio viendo si cada palabra tien o no 
+	# voy a separar por cada espacio viendo si cada palabra tiene o no 
 	# un caracter numerico, caracter especial o palabras con tildes,
-	#  si lo tiene tira un mensaje de error 
-	if len(value)>=3:
-		# value = "ja asbhe"
-		for palabra in value.split(" "): #value.split(" ") me devuelve un array de palabras
+	# si lo tiene tira un mensaje de error 
 
-			if not len(palabra)>=3:
-				raise ValidationError("Debe ingresar más de 3 caracteres en cada palabra")
+	if len(value)>=3: #Controla que no se ingresen nombres cortos como ja"
+
+		palabras = value.split(" ")
+
+		for i, palabra in enumerate(palabras): #value.split(" ") me devuelve un array de palabras
+
+			if (i==0) and (not len(palabra)>=2):#Controla la 1er palabra para ingresar Ej: Di o Mc
+				raise ValidationError("Debe ingresar más letras")
+			if (i>0) and (not len(palabra)>=3): #Controla las siguientes palabras para ingresar Ej: Ana o Sol 
+					raise ValidationError("Debe ingresar más de 3 letras por espacio")
 
 			if not palabra.isalpha():
 				raise ValidationError("Debe ingresar solo letras")
@@ -19,7 +24,7 @@ def nombreValidator(value):
 			# me devuelve False si el string contiene los caracteres
 			# [0-9] o algun caracter especial como['#','.','-','á','é','í','ó','ú',etc] 
 	else:	
-		raise ValidationError("Debe ingresar más de 3 caracteres")
+		raise ValidationError("Debe ingresar más de 3 letras")
 	
 def isbnValidator(value):
 	if not value == abs(value):
