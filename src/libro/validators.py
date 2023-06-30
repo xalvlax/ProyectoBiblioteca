f83195e6a5457609698550085a2faf4605e4db33
xalvlax <sergiosalvatierra86@gmail.com>
2023-06-30 00:01:52 -0300

    FIX: Se corrige y modifica el validador de isbn en la app libro

--- src/libro/validators.py
+++ src/libro/validators.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+import re
 
 def nombreValidator(value):
 	# value va a tener todo el string que se introdujo en el input
@@ -27,8 +28,12 @@ def nombreValidator(value):
 		raise ValidationError("Debe ingresar más de 3 letras")
 	
 def isbnValidator(value):
-	if not value == abs(value):
-		raise ValidationError("Debe ser un numero positivo")
-	value = str(value)
-	if not len(value)==13:
-		raise ValidationError("Debe tener 13 dígitos")
+
+	if len(value) != 13:
+		raise ValidationError("Volver a ingesar. El ISBN esta compuesto por 13 numeros")
+	
+	if not re.match('^[0-9]{13}$', value):
+		raise ValidationError("Debe ingresar solo numeros")
+	
+	if (value[0:3] != "978") and (value[0:3] != "979"):
+		raise ValidationError("El ISBN debe iniciar con 978 o 979")
